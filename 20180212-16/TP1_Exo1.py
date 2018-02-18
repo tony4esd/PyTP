e01c00208607177fc10be8116443b3c44a413b9a
tony4esd <tony4esd.aston@gmail.com>
2018-02-18 10:45:50 +0100

    Add TP1_Exo1.py file

---
+++ 20180212-16/TP1_Exo1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+"""
+Exercice 1 :
+Ecrire un programme qui demande la saisie d’une phrase et affiche sa taille
+totale, le nombre de voyelles et consonnes et le nombre de mots.
+"""
+
+phrase = "Ceci est une phrase"
