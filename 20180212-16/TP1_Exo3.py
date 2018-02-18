0b6c08f0e4cf842a0f8b957b6e06cfbf844398e4
tony4esd <tony4esd.aston@gmail.com>
2018-02-18 19:01:36 +0100

    File TP1_Exo3.py created.

---
+++ 20180212-16/TP1_Exo3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+'''
+Exercice 3 :
+Ecrire un programme qui affiche le dictionnaire suivant {‘r’:3,’a’:0,’y’:5,’z’:1,’t’:4,’e’:2}
+trié par clé ( de a à z) et trié par valeur (de 0 à 5)
+'''
