3af70cc871d4fd90c88fde3a33b429967bd3672f
tony4esd <tony4esd.aston@gmail.com>
2018-02-20 00:31:42 +0100

    TP1_Exo4.py finished

--- 20180212-16/TP1_Exo4.py
+++ 20180212-16/TP1_Exo4.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
+
 '''
 Exercice 4:
 Ecrire un programme qui affiche un menu en boucle permettant d’ajouter un nouveau contact,
@@ -19,29 +20,33 @@ class Contacts:
 
     obj_id = 0
 
-    def __init__(self):
+    def __init__(self, nom, prenom, tel, mail):
         self.id = Contacts.obj_id
-        self.nom = ""
-        self.prenom = ""
-        self.tel = ""
-        self.mail = ""
-        Contacts.obj_id += 1
-
-    def ajout(self, nom, prenom, tel, mail):
         self.nom = nom
         self.prenom = prenom
         self.tel = tel
         self.mail = mail
+        Contacts.obj_id += 1
 
     def afficheContact(self):
         print(
-            "\n---Contact ID {}---\n"
+            "\n---Contact N° {}---\n"
             "\n"
             "Nom : {}\n"
             "Prenom : {}\n"
             "Telephone : {}\n"
             "Adresse Mail : {}\n"
-            "\n".format(self.id, self.nom, self.prenom, self.tel, self.mail))
+            "\n".format(self.id+1, self.nom, self.prenom, self.tel, self.mail))
+
+
+def ajoutContact():
+    nom = input("Nom: ")
+    prenom = input("Prenom: ")
+    tel = input("Telephone: ")
+    mail = input("Adresse Mail: ")
+
+    ctact = Contacts(nom, prenom, tel, mail)
+    return(ctact)
 
 
 def afficheMenu():
@@ -51,10 +56,22 @@ def afficheMenu():
         "1) Ajout d'un nouveau contact\n"
         "2) Afficher tous les contacts\n"
         "3) Quitter le programme\n"
-        "\n"
-        "Faite un choix :")
+        "\n")
 
 
-a = Contacts()
-a.ajout(nom="hasbroucq", prenom="Tony", tel="00.00.00.00.00", mail="test@test.com")
-a.afficheContact()
+contact = []
+i = True
+while i:
+    afficheMenu()
+    choix = input("Faite votre choix: ")
+    while choix not in '123':
+        choix = input("Faite un choix parmis 1, 2 ou 3")
+    if choix == '1':
+        ctact = ajoutContact()
+        contact.append(ctact)
+        print("Contact ajoute\n")
+    if choix == '2':
+        for j in contact:
+            j.afficheContact()
+    if choix == '3':
+        i = False
